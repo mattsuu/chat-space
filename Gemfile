e42f523b9b3f66fe0946f0637a11217633eff928
mattsuu <10ssss12s@gmail.com>
2019-12-09 16:56:59 +0900

    修正

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ gem 'haml-rails'
 gem 'font-awesome-sass'
 gem 'devise'
 gem 'carrierwave'
+gem 'fog-aws'
 gem 'mini_magick'
 group :production do
   gem 'unicorn', '5.4.1'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   process resize_to_fit: [400, 200]
   # Choose what kind of storage to use for this uploader:
+  storage :fog
   # storage :file
   # storage :fog
 
